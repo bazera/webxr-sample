ff93b504fe6abf23b457049ec3bdc25534b10f86
bazera <bazera.g@gmail.com>
2022-06-27 17:44:51 +0400

    fix: remove packages, add CDN

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import * as THREE from './node_modules/three/build/three.module.js';
+import * as THREE from 'three';
 
 document.addEventListener('DOMContentLoaded', () => {
   const initialize = async () => {
